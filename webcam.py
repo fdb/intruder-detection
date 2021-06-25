8ceaaf3e8d3676544fb8431699f01dd508640ef3
Frederik De Bleser <frederik@debleser.be>
2021-06-25 16:35:25 +0200

    State managment

--- webcam.py
+++ webcam.py
@@ -10,64 +10,81 @@ LAMP_ON_URL = 'https://maker.ifttt.com/trigger/lamp_on/with/key/bRWG-Z9VFdWKcG1_
 LAMP_OFF_URL = 'https://maker.ifttt.com/trigger/lamp_off/with/key/bRWG-Z9VFdWKcG1_GlNpS9'
 
 last_event_time = datetime.datetime.now()
-intruder_detected = False
+
+STATE_NOBODY_HERE = "nobody_here"
+STATE_INTRUDER_DETECTED = "intruder_detected"
+
+state = STATE_NOBODY_HERE
+
+
+PIXEL_CHANGE_THRESHOLD = 50_000
+
 
 def turn_led_on():
     system('/opt/vc/bin/vcmailbox 0x00038041 8 8 130 1')
 
+
 def turn_led_off():
     system('/opt/vc/bin/vcmailbox 0x00038041 8 8 130 0')
 
+
 def show_webcam(mirror=False):
-    global last_event_time
-    global intruder_detected
+    global state
     cam = cv2.VideoCapture(0)
     prev_img = None
+    skip_frames = 0
     while True:
         ret_val, img = cam.read()
-        if mirror: 
+        if skip_frames > 0:
+            prev_img = img
+            skip_frames -= 1
+            continue
+        if mirror:
             img = cv2.flip(img, 1)
         if prev_img is None:
             prev_img = img
 
         delta = cv2.subtract(prev_img, img)
         thresh = cv2.threshold(delta, 10, 255, cv2.THRESH_BINARY_INV)[1]
-        count =  np.count_nonzero(thresh)
+        count = np.count_nonzero(thresh)
         changed_pixels = thresh.size - count
+        print(changed_pixels)
 
         now = datetime.datetime.now()
-        time_delta = (now - last_event_time).seconds
 
-        #print(thresh.size)
-        #print(thresh.size - count)
-        if changed_pixels > 20_000 and not intruder_detected and time_delta > 1:
-            print("INTRUDER ALERT")
-            turn_led_on()
-            requests.post(LAMP_OFF_URL)
-            intruder_detected = True
-            last_event_time = now
-        elif changed_pixels < 20_000 and time_delta > 10:
-            print("NOBODY THERE")
-            turn_led_off()
-            requests.post(LAMP_ON_URL)
-            intruder_detected = False
-            last_event_time = now
+        if state == STATE_NOBODY_HERE:
+            if changed_pixels > PIXEL_CHANGE_THRESHOLD:
+                state = STATE_INTRUDER_DETECTED
+                print("INTRUDER ALERT")
+                turn_led_off()
+                requests.post(LAMP_OFF_URL)
+                timer_start = now
 
-        #print(thresh.size - count)
-        #if thresh.size 
-        
+        elif state == STATE_INTRUDER_DETECTED:
+            time_delta = (now - timer_start).seconds
+            if time_delta > 10:
+                if changed_pixels > PIXEL_CHANGE_THRESHOLD:
+                    print("...INTRUDER IS STILL HERE...")
+                    timer_start = now
+                else:
+                    print("NOBODY HERE")
+                    turn_led_on()
+                    requests.post(LAMP_ON_URL)
+                    skip_frames = 100
+                    prev_img = img
+                    state = STATE_NOBODY_HERE
+
         cv2.imshow('my webcam', delta)
-        if cv2.waitKey(1) == 27: 
+        if cv2.waitKey(1) == 27:
             break  # esc to quit
 
         prev_img = img
-
 
     cv2.destroyAllWindows()
 
 
 def main():
-    turn_led_off()
+    turn_led_on()
     requests.post(LAMP_ON_URL)
     show_webcam(mirror=True)
 
